4c1de42425a941ff4a3d3b7b66c81f9198981dfc
wumingqing2018 <759823845@qq.com>
2019-12-17 10:15:02 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch 1.13
      Changes to be committed:
     	modified:   backEnd/__pycache__/urls.cpython-36.pyc
     	modified:   padManage/__pycache__/urls.cpython-36.pyc
     	modified:   padManage/urls.py
     	renamed:    templates/search/indexes/models/Pingbanleibiao_text.txt -> templates/search/indexes/backEnd/Pingbanleibiao_text.txt
     	renamed:    whoosh_index/MAIN_8qolo26a2euoql0q.seg -> whoosh_index/MAIN_wxk9d78iwzcck508.seg
     	modified:   whoosh_index/_MAIN_1.toc

--- padManage/urls.py
+++ padManage/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from backEnd import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
